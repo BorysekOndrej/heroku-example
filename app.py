6585a57544e45adbf6a5704255dafae78b98e77d
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-10-29 17:15:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<name>")
 def hello_there(name: str = "General Kenobi"):
-    return f"<p>Hello, {name}!</p>"
+    return f"<p>Hello there, {name}!</p>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
